f40539d8ac3521377842ee9e963184386c46e813
bakus <bakus@www3236u.sakura.ne.jp>
2011-04-13 23:53:57 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommandMail::Application.config.secret_token = '7798b179794eeb5a3a2351ce3cb1bdabb447d7191e181e4c5b5791299d7e0d93de9c22ce0494fc86a4aaf327688d518814abc8e707d748d596478eac44491ede'
